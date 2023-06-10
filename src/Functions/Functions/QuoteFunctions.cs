ed03a3d559bf7b6c6baf4b8dc812029a276a631b
Chad Green <chadgreen@chadgreen.com>
2023-06-10 10:17:35 -0400

    Adding random quote generator

--- src/Functions/Functions/QuoteFunctions.cs
+++ src/Functions/Functions/QuoteFunctions.cs
@@ -20,7 +20,8 @@ public class QuoteFunctions
 	}
 
 	[Function("GetQuoteList")]
-	public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData request)
+	public async Task<HttpResponseData> RunAsync(
+		[HttpTrigger(AuthorizationLevel.Function, "get", Route = "quotes")] HttpRequestData request)
 	{
 		_logger.LogInformation("GetQuoteList - Getting list of quotes");
 		return await request.CreateResponseAsync(await QuoteServices.GetQuoteListAsync(_sqlContext), _jsonSerializerOptions);
@@ -31,8 +32,31 @@ public class QuoteFunctions
 		[HttpTrigger(AuthorizationLevel.Function, "get", Route = "quotes/{id}")] HttpRequestData request,
 		string id)
 	{
-		_logger.LogInformation("GetQuoteById - Getting quote by id {id}", id);
-		return await request.CreateResponseAsync(await QuoteServices.GetQuoteAsync(_sqlContext, Convert.ToInt32(id)), _jsonSerializerOptions);
+		try
+		{
+			ArgumentNullException.ThrowIfNull(id);
+			if (id.ToUpperInvariant() == "RANDOM")
+			{
+				_logger.LogInformation("GetQuoteById - Getting quote by id {id}", id);
+				return await request.CreateResponseAsync(await QuoteServices.GetRandomQuoteAsync(_sqlContext), _jsonSerializerOptions);
+			}
+			else
+			{
+				_logger.LogInformation("GetQuoteById - Getting random quote", id);
+				if (!int.TryParse(id, out int quoteId))
+					throw new ArgumentException("The id value must be numeric.");
+				return await request.CreateResponseAsync(await QuoteServices.GetQuoteAsync(_sqlContext, quoteId), _jsonSerializerOptions);
+			}
+		}
+		catch (Exception ex) when (ex is ArgumentNullException || ex is ArgumentException)
+		{
+			return request.CreateBadRequestResponse(ex);
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError("{FunctionName} - Unexpected exception: {ExceptionMessage}", nameof(GetQuoteByIdAsync), ex.Message);
+			return request.CreateErrorResponse(ex);
+		}
 	}
 
 }
--- src/Services/Services/QuoteServices.cs
+++ src/Services/Services/QuoteServices.cs
@@ -7,9 +7,21 @@ public static class QuoteServices
 {
 
 	public static async Task<QuoteListResponse> GetQuoteListAsync(SqlContext sqlContext)
-		=> (await sqlContext.Quotes.Include(x => x.Author).AsNoTracking().ToListAsync()).ToListResponse();
+		=> (await GetListAsync(sqlContext)).ToListResponse();
 
 	public static async Task<QuoteResponse?> GetQuoteAsync(SqlContext sqlContext, int quoteId)
-		=> (await sqlContext.Quotes.Include(x => x.Author).AsNoTracking().FirstOrDefaultAsync(x => x.QuoteId == quoteId)).ToResponse();
+		=> (await GetAsync(sqlContext, quoteId)).ToResponse();
+
+	public static async Task<QuoteResponse?> GetRandomQuoteAsync(SqlContext sqlContext)
+		=> GetRandomAsync(await GetListAsync(sqlContext)).ToResponse();
+
+	private static async Task<List<Quote>> GetListAsync(SqlContext sqlContext)
+		=> await sqlContext.Quotes.Include(x => x.Author).AsNoTracking().ToListAsync();
+
+	private static async Task<Quote?> GetAsync(SqlContext sqlContext, int quoteId)
+		=> await sqlContext.Quotes.Include(x => x.Author).AsNoTracking().FirstOrDefaultAsync(x => x.QuoteId == quoteId);
+
+	private static Quote? GetRandomAsync(List<Quote> quoteList)
+		=> quoteList[new Random().Next(quoteList.Count)];
 
 }
